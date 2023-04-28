c25e5ff3e22dd017408bf5c43db205cddf44c121
Rafael Mayor Alberto <rafaelmayor22@gmail.com>
2023-04-27 22:43:10 -0400

    add episode mockup

--- src/components/episodes/EpisodesDescription.tsx
+++ src/components/episodes/EpisodesDescription.tsx
@@ -9,7 +9,16 @@ const EpisodesDescription: FC<EpisodesDescriptionProps> = ({
   return (
     <div className="episode-descriptions">
       <h1>{title}</h1>
-      <pre>{description}</pre>
+
+      <div className="description" dangerouslySetInnerHTML={{ __html: description }} />
+
+      <audio controls>
+        <source src={url} type="audio/mp3" />
+        <p>
+          Your browser doesn't support this audio file. Here is a
+          <a href="url">link to the audio</a> instead.
+        </p>
+      </audio>
     </div>
   );
 };
