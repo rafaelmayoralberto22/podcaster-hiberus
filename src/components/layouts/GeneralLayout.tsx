056a32080b83c57b076f1bc3f2361c5ebe8d8701
Rafael Mayor Alberto <rafaelmayor22@gmail.com>
2023-05-04 23:20:58 -0400

    remove PorpsWithChildren in GeneralLayout

--- src/components/layouts/GeneralLayout.tsx
+++ src/components/layouts/GeneralLayout.tsx
@@ -1,11 +1,10 @@
-import { FC, PropsWithChildren } from "react";
 import { Outlet } from "react-router-dom";
 import { useLoadingLocation } from "../../helpers/hooks/useLoadingLocation";
 import { RouterLoading } from "../loading/RouterLoading";
 import GeneralContent from "./content/GeneralContent";
 import Header from "./header/Header";
 
-const GeneralLayout: FC<PropsWithChildren> = () => {
+const GeneralLayout = () => {
   const { loading } = useLoadingLocation();
 
   return (
