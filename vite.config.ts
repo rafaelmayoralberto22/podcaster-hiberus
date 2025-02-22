a1a354c5c466435b3d0b314fb56855552a826332
Rafael Mayor Alberto <rafaelmayor22@gmail.com>
2025-02-22 17:30:36 +0100

    feat(home): add home page

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/podcaster/",
   plugins: [react()],
   test: {
     environment: "jsdom",
